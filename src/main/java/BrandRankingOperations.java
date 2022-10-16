daf6ce22220bba6d835bfba6c2f9438703661365
Lilija Cipiseva <lilija.cipiseva@gmail.com>
2022-10-16 09:40:23 +0100

    1MD --Create Key-value DB with DBMS Redis

--- src/main/java/BrandRankingOperations.java
+++ src/main/java/BrandRankingOperations.java
@@ -16,26 +16,31 @@ public class BrandRankingOperations {
                 jedis.flushAll(); //Deleting everything from Data Base
 
                 //1. Izveidot skriptu, kas datu bāzē ievieto vismaz 500 ierakstus (var arī no faila).
+                System.out.println("1. Izveidot skriptu, kas datu bāzē ievieto vismaz 500 ierakstus\n");
                 //Uploading records to DB from csv file
                 List<String> keys = create(jedis, getCsvFileByName("BrandRanking"));
                 //Checking that 500 records in Data Base
                 assert jedis.dbSize() == 500 : "Unexpected number of rows in Database";
 
                 //2. Izveidot skriptu, kas maina vairākus ierakstus (10-100).
+                System.out.println("2. Izveidot skriptu, kas maina vairākus ierakstus (10-100).\n");
                 //Updating brand value for brands form 100 till 200 position
-                update(jedis, keys.subList(99, 120));
+                update(jedis, keys.subList(100, 200));
                 //Checking that 500 records in Data Base
                 assert jedis.dbSize() == 500 : "Unexpected number of rows in Database";
 
                 //3. Izveidot skriptu, kas atlasa vairākus ierakstus (10-100).
+                System.out.println("3. Izveidot skriptu, kas atlasa vairākus ierakstus (10-100).\n");
                 //Selecting 10 records with mget() command
-                System.out.println(jedis.mget("Apple", "Amazon"
+                System.out.println("10 records selected with mget() command: " + jedis.mget("Apple", "Amazon"
                         , "Google", "Microsoft", "Walmart", "Samsung Group"
-                        , "Facebook", "ICBC", "Huawei", "Verizon"));
+                        , "Facebook", "ICBC", "Huawei", "Verizon") + "\n");
                 //Selecting records, which contains 's' letter in brand name
-                System.out.println(jedis.mget(jedis.keys("*s*").toArray(new String[0])));
+                System.out.println("All records selected which contains 's' letter in brand name: "
+                        + jedis.mget(jedis.keys("*s*").toArray(new String[0])) + "\n");
 
                 //4. Izveidot skriptu, kas dzēš vairākus ierakstus (10-100).
+                System.out.println("4. Izveidot skriptu, kas dzēš vairākus ierakstus (10-100).\n");
                 // All records for brands with names starting with 'A' will be deleted
                 String pattern = "A*";
                 int recordCountWithPattern = jedis.keys(pattern).size();
@@ -62,6 +67,7 @@ public class BrandRankingOperations {
                 jedis.set(record.brand, record.toString());
                 keys.add(record.brand);
             }
+            System.out.println("Database is created. Records count: " + jedis.dbSize() + "\n");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -70,16 +76,19 @@ public class BrandRankingOperations {
 
     public static void update(Jedis jedis, List<String> keys) {
         Random random = new Random();
+        int i = 1;
         for (String key : keys) {
             BrandRanking brandRanking = new BrandRanking(jedis.get(key));
-            System.out.println("Record before update: " + brandRanking);
+            System.out.println(i + " Record before update: " + brandRanking);
             brandRanking.brandValue = String.valueOf(random.nextInt(2000000));
             brandRanking.previousBrandValue = String.valueOf(random.nextInt(2000000));
             brandRanking.rating = "AAA";
             brandRanking.previousRating = "AA";
             jedis.set(key, brandRanking.toString());
-            System.out.println("Record after update: " + new BrandRanking(jedis.get(key)));
+            System.out.println(i + " Record after update: " + new BrandRanking(jedis.get(key)));
+            i++;
         }
+        System.out.println("\nDatabase is updated (4 attributes). Records count: " + keys.size() + "\n");
     }
 
     public static void deleteRecordsByKeyPattern(Jedis jedis, String pattern) {
